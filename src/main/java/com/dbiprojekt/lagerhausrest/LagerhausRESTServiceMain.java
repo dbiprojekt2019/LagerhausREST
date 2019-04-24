ac69af097d05ab6928ca48823216047b025190a4
pseiringer <phil.seiringer@gmail.com>
2019-04-24 16:48:12 +0200

    Started to setup Project as a SpringBootApplication

---
+++ src/main/java/com/dbiprojekt/lagerhausrest/LagerhausRESTServiceMain.java
@@ -0,0 +1,11 @@
+package com.dbiprojekt.lagerhausrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagerhausRESTServiceMain {
+    public static void main(String[] args) {
+        SpringApplication.run(LagerhausRESTServiceMain.class);
+    }
+}
