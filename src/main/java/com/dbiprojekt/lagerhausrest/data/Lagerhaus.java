58f43697bbe33c7441502df16981e7a19a1e705c
pseiringer <phil.seiringer@gmail.com>
2019-06-17 19:29:06 +0200

    --- Updated --- removed unused classes removed unused bean from AppConfiguration

--- src/main/java/com/dbiprojekt/lagerhausrest/data/Lagerhaus.java
+++
@@ -1,9 +0,0 @@
-package com.dbiprojekt.lagerhausrest.data;
-
-import lombok.Data;
-
-@Data
-public class Lagerhaus {
-    private int lagerId;
-    private String bezeichnung;
-}
--- src/main/java/com/dbiprojekt/lagerhausrest/manager/database/StoredProcedure.java
+++
@@ -1,7 +0,0 @@
-package com.dbiprojekt.lagerhausrest.manager.database;
-
-public enum StoredProcedure {
-    GET_ALL_LAGERHAUS,
-    GET_LIEFERUNG_BY_ID,
-    GET_REIFEGRAD_BY_ID
-}
