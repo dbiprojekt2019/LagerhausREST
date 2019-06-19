7d23b65f1517fad05f3fcb1529380b307fbdf389
pseiringer <phil.seiringer@gmail.com>
2019-06-19 08:36:08 +0200

    --- Updated --- optimised inputs

--- src/main/java/com/dbiprojekt/lagerhausrest/data/FullHarvestMonth.java
+++ src/main/java/com/dbiprojekt/lagerhausrest/data/FullHarvestMonth.java
@@ -1,14 +1,8 @@
 package com.dbiprojekt.lagerhausrest.data;
 
-import com.dbiprojekt.lagerhausrest.data.databasePOJOs.Delivery;
 import com.dbiprojekt.lagerhausrest.data.databasePOJOs.GrowingArea;
 import com.dbiprojekt.lagerhausrest.data.databasePOJOs.HarvestMonth;
 import lombok.Data;
-
-import java.sql.Date;
-import java.util.List;
-
-//import java.util.Date;
 
 @Data
 public class FullHarvestMonth {
